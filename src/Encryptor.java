2083353039443c856ca985f8da8fc6c9df5c4335
Alena <alena.zhukova1506@gmail.com>
2024-05-07 20:33:05 +0300

    wrote algorithm of encryption

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -6,37 +6,36 @@ import java.util.*;
 
 public class Encryptor {
 
-    List<String> listOfAllEncryptions = new ArrayList<>();
+    public static List<String> listOfAllEncryptions = new ArrayList<>();
 
-    Map<Character, List<String>> encryptionMap = new HashMap<>();
+    public static Map<Character, List<String>> encryptionMap = new HashMap<>();
 
     public static FileEncryptor fileEncryptor = null;
     FileDecryptor fileDecryptor = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Выберите операцию:"
                 + "\n0 - зашифровать файл"
                 + "\n1 - расшифровать файл");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String command = null;
-        try {
-            command = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String command = reader.readLine();
 
-        if (command.equals("0")) {
-            //зашифровать файл
-            System.out.println("Введите имя файла, который нужно зашифровать");
-            Scanner scanner = new Scanner(System.in);
-            String fileToEncrypt = scanner.nextLine();
-            fileEncryptor = new FileEncryptor(Path.of(fileToEncrypt));
-            fileEncryptor.encrypt();
-        } else if (command.equals("1")) {
-            // расшифровать файл
-        } else {
-            System.out.println("Вы ввели неверную команду");
+            if (command.equals("0")) {
+                //зашифровать файл
+                System.out.println("Введите абсолютный путь файла, который нужно зашифровать:");
+                String fileToEncrypt = reader.readLine();
+                System.out.println("Выберите сложность шифрования от 1 до 5:");
+                int complexity = Integer.valueOf(reader.readLine());
+                System.out.println("Введите абсолютный путь для нового файла:");
+                String pathToNewFile = reader.readLine();
+                fileEncryptor = new FileEncryptor(Path.of(fileToEncrypt), Path.of(pathToNewFile), complexity);
+                fileEncryptor.encrypt();
+            } else if (command.equals("1")) {
+                // расшифровать файл
+            } else {
+                System.out.println("Вы ввели неверную команду");
+            }
         }
 
     }
--- src/FileEncryptor.java
+++ src/FileEncryptor.java
@@ -1,18 +1,71 @@
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileEncryptor {
 
-    Path fileToEncrypt = null;
+    Path fileToEncrypt;
+    Path pathToNewFile;
+    int complexity;
 
-    public FileEncryptor(Path fileToEncrypt) {
+    final String LIST_OF_CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    public FileEncryptor(Path fileToEncrypt, Path pathToNewFile, int complexity) {
         this.fileToEncrypt = fileToEncrypt;
+        this.pathToNewFile = pathToNewFile;
+        this.complexity = complexity;
     }
 
     public Path encrypt() {
 
+        Path encryptedFile = null;
+        if (!Files.exists(pathToNewFile)) {
+            try {
+                encryptedFile = Files.createFile(pathToNewFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+        try (FileReader in = new FileReader(fileToEncrypt.toFile());
+        FileWriter out = new FileWriter(pathToNewFile.toFile())) {
 
-        return null;
+            while (in.ready()) {
+                char newChar = (char) in.read();
+
+                if (!Encryptor.encryptionMap.containsKey(newChar)) {
+                    List<String> codes = new ArrayList<>();
+
+                    String code = null;
+                    while (code == null) {
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        for (int i = 0; i < complexity; i++) {
+                            stringBuilder.append(LIST_OF_CHARACTERS.charAt((int) (Math.random() * LIST_OF_CHARACTERS.length())));
+                        }
+                        code = stringBuilder.toString();
+
+                        if (!Encryptor.listOfAllEncryptions.contains(code)) {
+                            Encryptor.listOfAllEncryptions.add(code);
+                        } else {
+                            code = null;
+                        }
+                    }
+                    codes.add(code);
+                    Encryptor.encryptionMap.put(newChar, codes);
+                    out.write(code);
+                } else {
+                    out.write(Encryptor.encryptionMap.get(newChar).get(0));
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Файл не найден");
+        }
+
+        return pathToNewFile;
     }
 }
 
